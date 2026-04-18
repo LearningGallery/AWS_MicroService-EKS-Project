b892b0521fc2dbb27eea453af363a11f047acee5
AbuTalha Mulla <abutalha@amityonline.com>
2026-04-18 19:37:49 +0800

    first Commit

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -1,70 +1,70 @@
 
 variable "region" {
   description = "AWS region"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc-name" {
   description = "VPC Name for our Jumphost server"
-  type = string
-  default = "Jumphost-vpc"
+  type        = string
+  default     = "learninggallery"
 }
 
 variable "igw-name" {
   description = "Internet Gate Way Name for our Jumphost server"
-  type = string
-  default = "Jumphost-igw"
+  type        = string
+  default     = "learninggallery-igw"
 }
 
 variable "subnet-name1" {
   description = "Public Subnet 1 Name"
-  type = string
-  default = "Public-Subnet-1"
+  type        = string
+  default     = "sb-learninggallery-ie-1"
 }
 
 variable "subnet-name2" {
   description = "Subnet Name for our Jumphost server"
-  type = string
-  default = "Public-subnet2"
+  type        = string
+  default     = "sb-learninggallery-ie-2"
 }
 
 # Private subnet name variables
 variable "private_subnet_name1" {
   description = "Private Subnet 1 Name"
-  type = string
-  default = "Private-subnet1"
+  type        = string
+  default     = "sb-learninggallery-ia-1"
 }
 
 variable "private_subnet_name2" {
   description = "Private Subnet 2 Name"
-  type = string
-  default = "Private-subnet2"
+  type        = string
+  default     = "sb-learninggallery-ia-2"
 }
 
 variable "rt-name" {
   description = "Route Table Name for our Jumphost server"
-  type = string
-  default = "Jumphost-rt"
+  type        = string
+  default     = "rt-learninggallery"
 }
 
 variable "sg-name" {
   description = "Security Group for our Jumphost server"
-  type = string
-  default = "Jumphost-sg"
+  type        = string
+  default     = "sg-learninggallery"
 }
 
 
 variable "iam-role" {
   description = "IAM Role for the Jumphost Server"
-  type = string
-  default = "Jumphost-iam-role1"
+  type        = string
+  default     = "iam-learninggallery-role1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-098e39bafa7e7303d" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
@@ -74,9 +74,9 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  description = "EC2 keypair"
+  description = "learninggallery"
   type        = string
-  default     = "us-east-1"
+  default     = "learninggallery"
 }
 
 variable "instance_name" {
